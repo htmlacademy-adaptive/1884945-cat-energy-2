930360841836e5b5c952c667b2e5253e75c8b0ab
Pandenimik <89628864+Pandenimik@users.noreply.github.com>
2024-11-21 15:14:30 +0300

    Добавляет векторную графику (#4)
    * Добавляет спрайт и подклчает иконки в advantages и socials
    
    * Подключает основные иконки.
    
    * Добавляет кастомные checkbox и radio. Добавляет состояния интерактивным элементам.
    
    * Исправляет контентый вектор. Добавляет compare-slider.
    
    * Добавляет иконки в programms
    
    * Вносит правки по замечаниям

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,23 @@
-/* в этот файл добавляет скрипты*/
+
+// Burger nav-menu
+const button = document.querySelector('.burger-toggle');
+const navList = document.querySelector('.main-nav__list');
+const toggleNav = function () {
+  navList.classList.toggle('main-nav__list--closed');
+  button.classList.toggle('opened-nav');
+};
+
+button.addEventListener('click', (e) => {
+  e.stopPropagation();
+  toggleNav();
+});
+
+// Compare slider
+const compareSlider = document.querySelector('.compare-slider__range');
+const compareSlide = function () {
+  const slideValue = compareSlider.value;
+  document.querySelector('.compare-slider__img--before').style.clipPath = `polygon(0 0, ${ slideValue }% 0, ${ slideValue }% 100%, 0 100%)`;
+  document.querySelector('.compare-slider__img--after').style.clipPath = `polygon(${ slideValue }% 0, 100% 0, 100% 100%, ${ slideValue }% 100%)`;
+};
+
+compareSlider.addEventListener('input', compareSlide);

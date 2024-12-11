28005dc7afe449d2687eb644d716af13f7ce0cd0
Pandenimik <89628864+Pandenimik@users.noreply.github.com>
2024-12-11 16:26:44 +0300

    Добавляет сетки для всех страниц.
    * Добавляет сетки для header, intro, programs.
    
    * Добавляет сетки для advantages.
    
    * Добавляет сетки для example.
    
    * Добавляет сетки для footer.
    
    * Вносит правки по замечаниям module6-task1 по index.html
    
    * Добавляет Яндекс карту.
    
    * Правит по замечаниям линтера.
    
    * Доделывает яндекс карту
    
    * Вносит правки для Pixel Perfect мобильной версии index.html
    
    * Правки по замечаниям.
    
    * Добавляет меню навингации в поток и скрывает бургер-кнопку при отключении Javascript.
    
    * Добавляет правки для pixel prfect всех версий index.html
    
    * Правит conatiner, добавляет сетки для page__title.
    
    * Вносит правки по --no-js элементам.
    
    * Добавляет сетки во все версии catalog.html до main-products включительно.
    
    * Добавляет контейнер центровщик для контактов.
    
    * Добавляет сетки плюс pixel perfect для всех версий catalog.html
    
    * Добавляет сетки для form.html
    
    * Правит ошибки в отображении fieldset
    
    * Добавляет свойства для label c input при переполнении текстом
    
    * Правит замечания по преполнению.
    
    * Правит реализацию background-color под background-image.
    
    * Правит под переполнение плашку с адресом над картой.
    
    * Добавляет мелкие правки pixel perfect.
    
    * Добавляет переполнение checkbox и radio
    
    *  Добавляет правки по переполнению и доступности для блока example
    
    * Правит изменение размера input при печати в input type email.
    
    * Добавляет декоративные линни по ширине окна для catalog.html
    
    * Добавляет декоративные линии которые тянутся на всю ширину окна для form.html
    
    * Устраняет обрезание обводки при фокусе на checkbox и radio
    
    * Правит текст в advatages и example для Pixel perfect.
    
    * Правит именование переменных для цвета подложки раздела gift

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,7 +1,17 @@
 
 // Burger nav-menu
-const button = document.querySelector('.burger-toggle');
+const header = document.querySelector('.header');
+const headerLogo = document.querySelector('.header__logo--secondary');
+const nav = document.querySelector('.main-nav');
 const navList = document.querySelector('.main-nav__list');
+const button = document.querySelector('.burger-toggle');
+header.classList.remove('header--no-js');
+headerLogo.classList.remove('header__logo--no-js');
+nav.classList.remove('main-nav--no-js');
+navList.classList.remove('main-nav__list--no-js');
+button.classList.remove('burger-toggle--no-js');
+navList.classList.toggle('main-nav__list--closed');
+button.classList.toggle('opened-nav');
 const toggleNav = function () {
   navList.classList.toggle('main-nav__list--closed');
   button.classList.toggle('opened-nav');
@@ -18,6 +28,72 @@ const compareSlide = function () {
   const slideValue = compareSlider.value;
   document.querySelector('.compare-slider__img--before').style.clipPath = `polygon(0 0, ${ slideValue }% 0, ${ slideValue }% 100%, 0 100%)`;
   document.querySelector('.compare-slider__img--after').style.clipPath = `polygon(${ slideValue }% 0, 100% 0, 100% 100%, ${ slideValue }% 100%)`;
+  document.querySelector('.compare-slider__thumb').style.left = `${ slideValue }%`;
 };
 
-compareSlider.addEventListener('input', compareSlide);
+compareSlider?.addEventListener('input', compareSlide);
+
+// Map
+initMap();
+
+async function initMap() {
+  // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+
+  await window.ymaps3.ready;
+
+  const {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker} = window.ymaps3;
+
+
+  // Иницилиазируем карту
+  const map = new YMap(
+    // Передаём ссылку на HTMLElement контейнера
+    document.getElementById('map'),
+
+    // Передаём параметры инициализации карты
+    {
+      location: {
+        // Координаты центра карты
+        center: [30.322910, 59.939141],
+
+        // Уровень масштабирования
+        zoom: 14
+      }
+    }
+  );
+
+  // Если ширина окна больше 1439px смещаем центр карты для desktop версии
+  if (window.innerWidth > 767) {
+    map.setLocation({
+      center: [30.322510, 59.940900],
+      zoom: 14
+    });
+  }
+
+  if (window.innerWidth > 1439) {
+    map.setLocation({
+      center: [30.317800, 59.939030],
+      zoom: 16
+    });
+  }
+
+  // Добавляем слой для отображения схематической карты
+  map.addChild(new YMapDefaultSchemeLayer());
+
+  // Добавляем слой для отображения маркера
+  map.addChild(new YMapDefaultFeaturesLayer());
+
+  // Создаем и добавляем маркер
+  const markerElement = document.createElement('div');
+  markerElement.className = 'contacts__marker';
+
+  const marker = new YMapMarker(
+    {
+      coordinates: [30.323037, 59.938631],
+      draggable: false,
+      mapFollowsOnDrag: true
+    },
+    markerElement
+  );
+
+  map.addChild(marker);
+}
